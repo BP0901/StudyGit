55533d9a0b3b6d27761361653e1824c7b4317745
TranBaoPhuce <phuc423vn@gmail.com>
2021-11-15 16:47:02 +0700

    Them tich 2 so

--- source.cpp
+++ source.cpp
@@ -13,22 +13,19 @@ int main()
 	cin >> b;
 	cout << "Chon phep toan (+,-) ";
 	cin >> phepToan;
-<<<<<<< HEAD
 	if(phepToan == "-")
 		cout << "Hiệu 2 số: " << hieu(a,b);
-=======
 	if(phepToan == "+")
 		cout << "Tổng 2 số: " << tong(a,b);
->>>>>>> 7e719260c936beaa15497c4a3fb5b22f72b1c88f
+	if(phepToan == "*A")
+		cout << "Tổng 2 số: " << tong(a,b);
 	system("pause");
 	return 0;
 }
-
-<<<<<<< HEAD
+int tich(int a, int b){
+	return a * b;
 int hieu(int a, int b){
 	return a - b;
-=======
 int tong(int a, int b){
 	return a + b;
->>>>>>> 7e719260c936beaa15497c4a3fb5b22f72b1c88f
 }
